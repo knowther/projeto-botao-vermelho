bec5ea31541ccdddde86d50d5c33b6df01aa8276
knowther <johnnywesley.contato@outlook.com>
2020-11-29 02:34:13 -0300

    Condições de aviso de acordo com o número de click, animação de explosão ao atingir 50 clicks

--- js/script.js
+++ js/script.js
@@ -1,12 +1,37 @@
 let nome = prompt("Qual seu nome?");
-document.getElementById("aviso").innerHTML = `Atenção ${nome}, você não pode clicar no botão!`
+document.getElementById("aviso").innerHTML = `Atenção ${nome}, você não pode clicar no botão!`;
 let conta = 0;
 document.getElementById("numeros").innerHTML = conta;
-
 function contador(){
     conta++;
-    document.getElementById("numeros").innerHTML = conta;
-}
+    document.getElementById("numeros").innerHTML = conta; 
+    if (conta == 10) {
+       alert(`Você já clickou ${conta} vezes, não está me ouvindo...`);
+    }
+    if (conta == 15) {
+      alert(`Você já clickou ${conta} vezes, não reclame se algo de arrado acontecer...`);
+   }
+   if (conta == 20) {
+      alert(`Cara, ${conta} vezes? Eu não vou falar mais nada.`);
+   }
+   if (conta == 21) {
+      alert(`${nome}, é sério, ${conta} vezes? você realmente não ouve as pessoas.`);
+   }
+   if (conta == 22) {
+      alert(`${nome}, eu acabei de falar com você, assim fica complicado...`);
+   }
+   if (conta == 23) {
+      alert(`Quer saber ${nome}? clicka mais, eu não vou me importar mais com isso!`);
+   }
+   if (conta == 40) {
+      alert(`Isso ${nome}, continue.`);
+   }
+   if (conta == 50) {
+      var explosion = document.getElementById("botaov").src = './img/explosion.png';
+      document.getElementById("aviso").innerHTML = `É ${nome}, você me destruiu, feliz agora? eu te avisei... :(`
+      }
+     
+   }
  function change1(){
     var img = document.getElementById("botaov").src = './img/button-pressed.png';
     
@@ -15,3 +40,4 @@ function contador(){
  function change2(){
    var img2 = document.getElementById("botaov").src = './img/button.png';
 }
+  
