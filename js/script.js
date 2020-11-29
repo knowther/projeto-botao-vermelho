760222dc3d77f9ed1fbad028f95fa804ec98f330
knowther <johnnywesley.contato@outlook.com>
2020-11-29 01:33:37 -0300

    Animação ao clickar no botão adicionada, modificação nas fontes e no alinhameto da contagem

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 let nome = prompt("Qual seu nome?");
-document.getElementById("demo").innerHTML = `Atenção ${nome}, você não pode clicar no botão!`
+document.getElementById("aviso").innerHTML = `Atenção ${nome}, você não pode clicar no botão!`
 let conta = 0;
 document.getElementById("numeros").innerHTML = conta;
 
@@ -8,9 +8,10 @@ function contador(){
     document.getElementById("numeros").innerHTML = conta;
 }
  function change1(){
-    var img = document.getElementById(botaov).src = './img/button-pressed.png';
+    var img = document.getElementById("botaov").src = './img/button-pressed.png';
+    
  }
 
  function change2(){
-    document.getElementById("botaov").scr="./img/button.png"
- }
+   var img2 = document.getElementById("botaov").src = './img/button.png';
+}
